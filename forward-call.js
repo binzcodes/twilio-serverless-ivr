d62f549edefaa023a6549f063b5735cfef957633
Ryan Binns <r@rbi.nz>
2022-01-30 07:25:55 +0700

    fix: rename function

--- forward-call.js
+++
@@ -1,6 +0,0 @@
-exports.handler = function(context, event, callback) {
-  let twiml = new Twilio.twiml.VoiceResponse()
-  twiml.say('Hello, this call is being forwarded.')
-  twiml.dial(context.MY_PHONE_NUMBER);
-  callback(null, twiml);
-};
